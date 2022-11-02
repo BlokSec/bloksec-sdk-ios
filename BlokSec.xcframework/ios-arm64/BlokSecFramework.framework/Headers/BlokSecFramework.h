8dd30494d7390b8d24197e1a4ed6fc28bd6d37d3
Mike Gillan <mike.gillan@gmail.com>
2022-11-02 07:32:55 -0400

    Initial commit of the BlokSec SDK for iOS

---
+++ BlokSec.xcframework/ios-arm64/BlokSecFramework.framework/Headers/BlokSecFramework.h
@@ -0,0 +1,19 @@
+//
+//  BlokSec.h
+//  BlokSec
+//
+//  Created by amit sharma on 12/09/20.
+//  Copyright © 2020 BlokSec Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlokSec.
+FOUNDATION_EXPORT double BlokSecVersionNumber;
+
+//! Project version string for BlokSec.
+FOUNDATION_EXPORT const unsigned char BlokSecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlokSec/PublicHeader.h>
+
+
